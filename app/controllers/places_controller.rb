a8a01e9411d1adc680dedd04eb61d73fb61348ea
Charlie <charlie@myjobreel.com>
2016-12-13 04:01:11 +0000

    Added Bootstrap 4 will_paginate

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,9 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+    # perform a paginated query:
+    @posts = Place.paginate(:page => params[:page], :per_page => 10)
+
   end
 
 end
